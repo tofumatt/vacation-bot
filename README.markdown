05c4b11b18a6fec2e17e6abf45e315a5787e62d0
Matthew Riley MacPherson <mmacpherson@mozilla.com>
2011-07-06 13:26:35 -0300

    Repo setup; add README

---
+++ README.markdown
@@ -0,0 +1,3 @@
+Do your IRC mates miss you while you're on vacation? Spin up an instance of
+vacation-bot and have it interject random sayings that sound like you. It'll
+be like you're still working (but, don't use it for that).
---
+++ conf/common.dist.js
@@ -0,0 +1,14 @@
+exports.conf = {
+	// IRC Config
+	jerk: {
+		// log: false,
+		channels: ['#yourchannel', '#yourotherchannel'],
+		nick: 'tofumatt',
+		user: {
+			// hostname: 'localhost',
+			realname: 'Yourname',
+			username: 'tofumatt'
+		},
+		server: 'irc.mozilla.org'
+	}
+}
