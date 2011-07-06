0db98348ef59c279ab51c83ba788b2d59a4054b9
Matthew Riley MacPherson <mmacpherson@mozilla.com>
2011-07-06 13:40:32 -0300

    Here are some default configs

--- conf/common.dist.js
+++ conf/common.dist.js
@@ -1,14 +1,25 @@
 exports.conf = {
-	// IRC Config
+	// IRC Config. If you're using a nick that's registered, be sure to
+	// include its password.
 	jerk: {
 		// log: false,
 		channels: ['#yourchannel', '#yourotherchannel'],
-		nick: 'tofumatt',
+		nick: 'yournick',
 		user: {
 			// hostname: 'localhost',
 			realname: 'Yourname',
-			username: 'tofumatt'
+			username: 'yournick'
 		},
-		server: 'irc.mozilla.org'
-	}
-}
+		// port: 6697,
+		server: 'irc.mozilla.org',
+		// ssl: true
+	},
+	// Phrases vacation-bot will randomly say.
+	phrases: [
+	    'Sure, I like Django, but Rails is better.',
+	],
+	// Phrases you will reply with when your IRC handle is mentioned.
+	replies: [
+	    'I am so not working; ask someone else!'
+	]
+};
